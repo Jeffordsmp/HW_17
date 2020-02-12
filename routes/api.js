0b002327abf98100548d00763b7667656de40911
Choop <matthewjeffords97@yahoo.com>
2020-02-12 16:09:30 -0700

    file structure

---
+++ routes/api.js
@@ -0,0 +1,16 @@
+const router = require("express").Router();
+const db = require("../models");
+
+router.post("/api/transaction", ({ body }, res) => {
+
+});
+
+router.post("/api/transaction/bulk", ({ body }, res) => {
+
+});
+
+router.get("/api/transaction", (req, res) => {
+
+});
+
+module.exports = router;
